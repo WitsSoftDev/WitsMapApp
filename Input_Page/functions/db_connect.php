1c9a7af3f3ecc1b0dbc73bf31868f4a8aabccec9
ick-seshoka <ick.seshoka@gmail.com>
2013-12-02 18:19:04 +0200

    added first version of the db_connect script

--- Input_Page/functions/db_connect.php
+++ Input_Page/functions/db_connect.php
@@ -1,4 +1,25 @@
 <?php
 
+    /**
+     *      This script is used to establish a connection o the database
+     */
+
+$host_name = "localhost";
+$user_name = "root";
+$password = "";
+$db_nama = "wits_map_app_db";
+
+//creating a connection
+$connection = mysqli_connect($host_name, $user_name, $password, $db_nama);
+
+//checking the connection status
+if(!$connection){
+    /*
+     *if the connection was unsuccessful display an error and kill the script
+     */
+    die("<h4>Unable to connect to the db</h4>");
+}else{
+    echo "<h4>Connected</h4>";
+}
 
 ?>
