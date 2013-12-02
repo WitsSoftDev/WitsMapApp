733d940f11ac1044dcf426b64844c8296614bec7
isaac <ick.seshoka@gmail.com>
2013-12-02 21:00:47 +0200

    moved the inputpage and nbproject folders
    Moved netbeaans project folders into one folder

---
+++ Input_Page_nb_project/Input_Page/pages/index.php
@@ -0,0 +1,34 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->
+<!--[if !IE]><!--><html lang="en"> <!--<![enfif]-->
+
+	<head>
+		<meta charset="utf-8">
+		<title>Data Input Page</title>
+
+		<!-- Mobile Specific Meta -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" >
+
+		<!-- Stylesheets -->
+		<link rel="stylesheet" type="text/css" href="../style/bootstrap.min.css">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	    <!--[if lt IE 9]>
+	      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+	      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+	    <![endif]-->
+
+	</head>
+	<body>
+		<div class="container">
+
+		</div>
+
+		<!-- JavaScript -->
+		<script type="text/javascript" src ="../js/jquery.min.js"></script>
+		<script type="text/javascript" src="../js/bootstrap.min.js"></script>
+	</body>
