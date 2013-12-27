60d3a9e32c669efad49d85bc10e17aafc5c4ddf8
TRex22 <jasonchalom@yahoo.co.uk>
2013-12-27 21:28:09 +0200

    Cleaned up develop and fixed LICENSE

--- Pravesh_Tutorial/GigGUIDE/gigguide/assets/www/js/constants.js
+++
@@ -1,2 +0,0 @@
-var constants_root = "http://197.85.191.92/gigguide/";
-var constants_maxTracks = 2; // Note actual number of 3 but used because of reference to array position
--- Pravesh_Tutorial/Readme.md
+++
@@ -1,4 +0,0 @@
-Pravesh's Tutorial
-------------------
-
-These are all the files which Pravesh used during his tutorial
