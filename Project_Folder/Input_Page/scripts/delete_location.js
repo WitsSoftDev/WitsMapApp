b35391f8cab6bc6e7e3f695ad5c948f08ad19d7e
ick-seshoka <ick.seshoka@gmail.com>
2014-01-04 19:23:55 +0200

    added UI update after the delete location buton is clicked

--- Project_Folder/Input_Page/scripts/delete_location.js
+++ Project_Folder/Input_Page/scripts/delete_location.js
@@ -13,11 +13,26 @@
           data: data,
 
                 success: function(data, textStatus, jqXHR) {
+                    
+                    var response = JSON.parse(data);
+                    var status = response["status"];
+                    var message = response["message"];
+                    
                     $("#my-modal-"+LocationID).modal('hide');
-                    $("div#location-delete-msg-content").html(data).hide()
+                    $("div#location-delete-msg-content").html(message).hide()
                        .fadeIn("slow")
                        .delay(3000)
                        .fadeOut("slow");
+               
+                    if(status === "success"){
+                        var current__num_of_locations = $("input#locations-count").val();
+                        //decrease current number by 1
+                        $("input#locations-count").val(current__num_of_locations-1);
+                    }
+                    
+                    
+                       
+                       
                 }
 
         });
